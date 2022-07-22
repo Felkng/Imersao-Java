9a8323fad6a616fc918a3483c6665614b01c93db
Felkng <105019177+Felkng@users.noreply.github.com>
2022-07-22 11:04:11 -0300

    Update clientHTTP.java

--- src/imersaojava/clientHTTP.java
+++ src/imersaojava/clientHTTP.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package imersaojava;
 
 import java.io.IOException;
@@ -12,7 +9,7 @@ import java.net.http.HttpResponse;
 
 /**
  *
- * @author Lipmon
+ * @author Felkng
  */
 public class clientHTTP {
     
